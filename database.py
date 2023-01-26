dbde612af4a1b8156bfe3c796b1b6df88cc09abd
Ilya Krivoshein <htctomsk634@gmail.com>
2023-01-26 15:35:19 +0700

    security fix

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
-SQLALCHEMY_DATABASE_URL = "postgresql://ngqhdcyg:KCodIF3ECwAoFPAxWLOF3V96IxpFKpU7@hattie.db.elephantsql.com/ngqhdcyg"
+DB = os.getenv('DB')
+SQLALCHEMY_DATABASE_URL = (f'{DB}')
 
 
 engine = create_engine(
